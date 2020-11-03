5a8dfba89bbaac6d7b1884fdb34d0d02993def3d
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-03 16:38:35 -0300

    Requisito 1: Jest Assincrono - teste da funcao answerPhone

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const answerPhone = require("../src/asyncJest");
+const answerPhone = require('../src/asyncJest');
 
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -10,13 +10,15 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+describe('o retorno do telefonema', () => {
+  test('atende', () => {
+    expect.assertions(1);
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', () => {
+    expect.assertions(1);
+    return expect(answerPhone(false)).rejects.toBe(
+      'Infelizmente não podemos atender...',
+    );
   });
 });
