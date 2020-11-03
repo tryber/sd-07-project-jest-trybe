feaba9a683964fc65c6b9971000c3dd3a69ea806
Fernando <nando.neves@gmail.com>
2020-11-03 15:20:50 -0300

    first requirement

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
+    await expect (answerPhone(true)).resolves.toBe('Oi!')
     // Insira seu teste assíncrono aqui
   });
-  test("ocupado", () => {
-    assert.fail();
+  test("ocupado", async () => {
+    await expect (answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...')
     // Insira seu teste assíncrono aqui
   });
 });
