57a594d8814cd085ef3bcf8a1324674103f5eb7b
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-03 15:06:28 -0300

    Commit para PR

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -14,6 +14,7 @@ describe("o retorno do telefonema", () => {
   test("atende", () => {
     assert.fail();
     // Insira seu teste assíncrono aqui
+    //Inserindo texto para PR
   });
   test("ocupado", () => {
     assert.fail();
