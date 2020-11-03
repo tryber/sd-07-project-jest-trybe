2960eb45075e75cf6691072c0d03dc2a249d22a8
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-03 15:18:15 -0300

    primeiro requisito

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    const response = await answerPhone(true)
+    expect(response).toBe('Oi!')
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    const response = await answerPhone(false)
+    expect(response).toBe('Infelizmente não podemos atender...')
   });
 });
