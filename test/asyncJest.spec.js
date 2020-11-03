20e762d854efc7ea9f263c0bd4639150f7d15b24
moacyrrln <moacyrrln@gmail.com>
2020-11-03 16:49:19 -0300

    1649

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -7,16 +7,14 @@ Dependendo do parâmetro o retorno da função varia, veja a função no arquivo
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'..
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende",  () => {
+    return expect(answerPhone(true)).resolves.toEqual('Oi!')
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...')
   });
 });
