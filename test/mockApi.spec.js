d6b37bee35f2e3ba7d1d6129bd5762c167da9b00
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-11-03 15:43:22 -0300

    Verificando segundo requisito corrigindo acento no Antonio

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -25,7 +25,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica o usuário', () => {
   const requestReturn = {
     gender: 'male',
-    name: {title: 'Mr', first: 'Antonio', last: 'Brito'},
+    name: {title: 'Mr', first: 'Antônio', last: 'Brito'},
     location: {country: 'Brazil',},
     email: 'tunico@bol.com.br',
     login: {username: 'tunicao123', password: '1234567890',},
