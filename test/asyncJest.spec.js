287f7783def290a2b76384b8a5e6fa40c73b1a5d
Iury Nathan <iurynathan@gmail.com>
2020-11-03 15:29:52 -0300

    requisito 4

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,6 +12,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => expect(answerPhone(true)).resolves.toEqual('Oi!'));
-  test("ocupado", () => expect(answerPhone()).rejects.toEqual('Infelizmente não podemos atender...'));
+  test("ocupado", () => expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...'));
   }
 );
