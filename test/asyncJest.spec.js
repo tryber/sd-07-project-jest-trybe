dcfd2b3ab8bcdcc70765b801a06b69d42304f59e
danielmlduarte <danielduarte.tryber@gmail.com>
2020-11-03 15:12:15 -0300

    Commit inicial - Resolvendo requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(true).then((response) => {
+      expect(response).toBe('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(false).catch((error) => {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    });
   });
 });
