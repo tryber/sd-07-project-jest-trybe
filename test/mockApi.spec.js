96e732fa7b618a09e4efd1b18753cdc08a848850
wellington <profwellington17@gmail.com>
2020-11-03 19:04:35 -0300

    fix2 requisito 2

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -37,15 +37,18 @@ const usuario = {
     password: '1234567890',
   }
 }
-api.fetchURL = jest.fn().mockResolvedValue(usuario);
-test('verifica se o usuário é o tunico', async () => {
-  return api.fetchURL().then((user) => {
-    expect(user.gender).toEqual('male');
-    expect(user.name.first).toEqual('Antônio');
-    expect(user.name.last).toEqual('Britto');
-    expect(user.location.country).toEqual('Brazil');
-    expect(user.email).toEqual('tunico@bol.com.br');
-    expect(user.login.username).toEqual('tunicao123');
-    expect(user.login.password).toEqual('1234567890');
+describe('verifica o usuário', () => {
+
+  api.fetchURL = jest.fn().mockResolvedValue(usuario);
+  test('verifica se o usuário é o tunico', async () => {
+    return api.fetchURL().then((user) => {
+      expect(user.gender).toEqual('male');
+      expect(user.name.first).toEqual('Antônio');
+      expect(user.name.last).toEqual('Britto');
+      expect(user.location.country).toEqual('Brazil');
+      expect(user.email).toEqual('tunico@bol.com.br');
+      expect(user.login.username).toEqual('tunicao123');
+      expect(user.login.password).toEqual('1234567890');
+    });
   });
 });
