68f53dfb824ff181371b10222d865e7122827f66
murilopereira006 <muriloedu006@gmail.com>
2020-11-03 18:38:25 -0300

    nao entendo o porque o teste finaliza antes do tempo de 3s do setTimeOut da funçao

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    answerPhone(true);
-    expect('Oi!');
+  test("atende", (done) => {
+    expect(answerPhone(true)).resolves.toBe('Oi!');
+    done();
   });
-  test("ocupado", () => {
-    answerPhone(false);
-    expect('Infelizmente não podemos atender...');
+  test("ocupado", (done) => {
+    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
+    done();
   });
 });
