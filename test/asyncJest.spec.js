6021ef8a1a6b6687a709c3d25aa94bed0c3acc52
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-11-03 16:02:00 -0300

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  
+  test("atende", async done => {
+    
+    expect(answerPhone(true)).resolves.toBe('Oi!')
+    done();
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async done => {
+
+    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...')
+    done();
   });
 });
