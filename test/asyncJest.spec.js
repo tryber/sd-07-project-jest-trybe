d718dcdaf467316518ef0d45515bd38b33e76db0
Felipe Nascimento <flpnascto@gmail.com>
2020-11-03 15:37:46 -0300

    Requisito 01.

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    expect.assertions(1);
+    await answerPhone(true).then(msg =>
+      expect(msg).toMatch('Oi!'))
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    expect.assertions(1);
+    await answerPhone(false).catch(msg =>
+      expect(msg).toMatch('Infelizmente não podemos atender...'))
   });
 });
