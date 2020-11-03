a2529ebd5b4644aaeb98a365c9f41c2bbd602773
Carol Andrade <a.carolg1@gmail.com>
2020-11-03 15:45:27 -0300

    requisito1 sub1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-
+    expect.assertions(1);
+    return answerPhone(true).then(answer => expect(answer).toEqual('Oi!'))
     // Insira seu teste assíncrono aqui
   });
   test("ocupado", () => {
-
+    expect.assertions(1);
+    return answerPhone(false).catch(error => expect(error).toEqual('Infelizmente não podemos atender...'))
     // Insira seu teste assíncrono aqui
   });
 });
