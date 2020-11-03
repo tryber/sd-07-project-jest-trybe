e987dd9b397613dfcdfc9939cd72dd78d3e79ac7
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-11-03 15:41:49 -0300

    Adicionado correção

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", done => {
-    expect(answerPhone(true)).toBeTruthy();
-    done();
+  test("atende", () => {
+    return expect(answerPhone(true)).resolves.toBeTruthy();
   });
-  test("ocupado", done => {
-    expect(answerPhone(false)).toBeFalsy();
-    done();
+  test("ocupado", () => {
+    return expect(answerPhone(false)).reject.toBe('Infelizmente não podemos atender...');
   });
 });
