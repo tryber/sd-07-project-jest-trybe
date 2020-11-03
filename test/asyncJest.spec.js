b1ce01af8a23fef265637a1f007565965b921812
Fernando <nando.neves@gmail.com>
2020-11-03 15:58:22 -0300

    segundo requerimento

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", async () => {
-    await expect (answerPhone(true)).resolves.toBe('Oi!')
-    // Insira seu teste assíncrono aqui
+    await expect (answerPhone(true)).resolves.toBe('Oi!'
   });
   test("ocupado", async () => {
     await expect (answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...')
-    // Insira seu teste assíncrono aqui
   });
 });
