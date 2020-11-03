bf76aa44d27c995599d816b082a5b9bcbf99eb39
Luise Rios <luisemiranda@gmail.com>
2020-11-03 15:44:24 -0300

    Project Jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then(resolve => 
+      expect(resolve).toEqual('Oi!'));
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch(error =>
+      expect(error).toEqual('Infelizmente não podemos atender...'));
   });
 });
