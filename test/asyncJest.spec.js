63795d5934e56d7d404e412d331b51dfde3eb660
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-11-03 18:26:39 -0300

    Adicionando requisito 4

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    expect.assertions(1);
     return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test("ocupado", () => {
-    expect.assertions(1);
     return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
