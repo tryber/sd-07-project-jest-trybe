60641406a2f4340089520657fc49ee75153c5638
PedroMarqDev <PedroMarqDev@gmail.com>
2020-11-04 12:00:11 -0300

    fix?

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,4 @@
 const mockFunctions = require('../src/mockFunctions');
-jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -13,7 +12,7 @@ O foco aqui é a utilização de mock functions.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+jest.mock('../src/mockFunctions');
 describe('verifica as funções e os mocks', () => {
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
