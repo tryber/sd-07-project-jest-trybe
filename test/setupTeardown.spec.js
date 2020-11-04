a59007df979d9e2cb7ecbef8166ca188957071b1
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-04 15:20:24 -0300

    Requisito 4 - Setup e Teardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,6 @@
+const { randomAttack } = require('../src/setupTeardown');
 const adventure = require('../src/setupTeardown');
+
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -20,7 +22,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => randomAttack());
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
