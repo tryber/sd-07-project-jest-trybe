c8f77f9f6ec5a117ea8b2c537cad179149129df5
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-04 19:52:53 -0300

    Alteraçoes do terceiro requisito

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,14 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", (done) => {
+  test("atende", () => {
     // Insira seu teste assíncrono aqui.
-    expect(answerPhone(true)).resolves.toBe('Oi!');
-    done();
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", (done) => {
+  test("ocupado", () => {
     // Insira seu teste assíncrono aqui
-    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
-    done();
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
