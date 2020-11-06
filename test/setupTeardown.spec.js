8c4fadf621e9a3536d16aef1bb139ad5d82cda78
CleberFontinele <cleberfontinele2387@gmail.com>
2020-11-06 09:30:57 -0300

    Setup e Teardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,21 +20,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
 
-  test('depois da primeira aventura', () => {
+  it('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
-  test('depois da segunda aventura', () => {
+  it('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
   });
-  test('depois da terceira aventura', () => {
+  it('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
   });
-  test('depois da quarta aventura', () => {
+  it('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
   });
-  test('depois da quinta aventura', () => {
+  it('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
 });
