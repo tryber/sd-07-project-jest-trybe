4c249b14766bbbf22232f43088cbd95e5476eab6
Lorena Góes <goesloren@gmail.com>
2020-11-06 13:39:11 -0300

    Requisito 1, refatorado

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", async (done) => {
-    await expect(answerPhone(true)).resolves.toBe('Oi!');
-    done();
+  test("atende", () => {
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", async (done) => {
-    await expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
-    done();
+  test("ocupado", () => {
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
