45f37f6a7b59034ac7caf201e1f063fc3abe5c59
Camila Valentim <camilaifpb@hotmail.com>
2020-11-07 08:23:38 -0300

    requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async (done) => {
+    await expect(answerPhone(true)).resolve.toBe('Oi!');
+    done();
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async (done) => {
+    await expect(answerPhone(false)).resolve.toBe('Infelizmente não podemos atender...');
+    done();
   });
 });
