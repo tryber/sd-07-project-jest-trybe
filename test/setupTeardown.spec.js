69c439a9b1bc98b1ed4ece60b92ac433af3f7f29
Camila Valentim <camilaifpb@hotmail.com>
2020-11-08 08:30:05 -0300

    correçao do nome beforeEach

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,5 +1,4 @@
 const adventure = require('../src/setupTeardown');
-
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -21,7 +20,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  beforEach(() => adventure.randomAttack());
+  beforeEach(() => adventure.randomAttack());
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
