d429097841e347e3e4dc52cd2695cbaaa16cfa09
afonsofn <afonsoafn@gmail.com>
2020-11-08 09:47:44 -0300

    Requesito 3

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,7 +15,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((num) => {
+    if (num === 0 || num === 1) {
+      return 1;
+    } else {
+    return num * mockFunctions.factorial(num - 1);
+    }
+  });
   
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
