d09fce85430ffe9eb05a671fcd9c8bf7df155663
vladimirrchagas <vladimirrchagas@gmail.com>
2020-11-08 16:11:32 -0300

    Analisando requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -7,9 +7,8 @@ Após cada aventura um de nossos aventureiros cairá.
 Cada um dos testes abaixo verifica a quantidade de aventureiros após cada iteração.
 Sua missão aqui é:
 
-  - Use a função randomAttack do objeto adventure
-  que remove um dos aventureiros toda vez que é chamada,
-  ela deve funcionar entre cada teste.
+  - Use a função randomAttack do objeto adventure que remove um dos aventureiros toda vez que é chamada, ela deve funcionar entre cada teste.
+
   Opcional:
   - Para ficar mais visível, imprima na tela após cada teste o grupo de aventureiros restante.
   - No fim dos testes, imprima uma mensagem com o nome do aventureiro que sobreviveu.
@@ -18,10 +17,22 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+beforeEach(() => {
+  adventure.randomAttack();
+  if (adventure.specialists.length > 1) {
+    // adventure.specialists.forEach(({ nome, classe }) => {
+    //   console.log(`Wow, Trybe Avanger ${nome} of class ${classe}, survive!!!`);
+    // });
+    console.log('See below the list of avengers that keep alive:');
+    console.table(adventure.specialists);
+  } else {
+    const { nome, classe } = adventure.specialists[0];
+    console.log(`Wow, The Last and M... F... Trybe Avanger ${nome} of class ${classe}, survive!!!`);
+  }
+});
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
