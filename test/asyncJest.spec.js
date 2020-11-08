9dc2b0823bc3b43997707d8406a1c7146813ef4b
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-08 13:28:34 -0300

    project init, first requirement

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async (done) => {
+    const response = await answerPhone(true);
+    expect(response).toBe('Oi!');
+    done();
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async (done) => {
+    await answerPhone(false)
+    .catch(answer => {
+      expect(answer).toBe('Infelizmente não podemos atender...');
+    })
+    done();
   });
 });
