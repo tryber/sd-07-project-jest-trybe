36eea17fe3348feded028dfd5a7edf0ee4f90cfc
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-11-09 11:49:05 -0300

    Alterando a implementação do test mock api

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -30,7 +30,7 @@ describe('verifica o usuário', () => {
     email: 'tunico@bol.com.br',
     login: {
     username: 'tunicao123',
-    password: 1234567890
+    password: '1234567890'
     }
   };
 
