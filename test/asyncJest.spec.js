72821ece694e810570d923add81a4ed2c23daff8
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-11-08 21:18:04 -0300

    asyncJest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,19 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    //assert.fail();
+    const answer = await answerPhone(true);
+    expect(answer).toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+
+  test("ocupado", async () => {
+    // assert.fail();
+    expect.assertions(1);
+    try {
+      await answerPhone();
+    } catch (error) {
+      expect(error).toMatch('Infelizmente não podemos atender...');
+    }
   });
 });
