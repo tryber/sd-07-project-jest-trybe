cd97fb1d8b3edb1c01615fea85e139bf196f9e8d
Thalys <thalysmatiascarrara@gmail.com>
2020-11-08 22:13:31 -0400

    requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,4 +1,5 @@
 const api = require('../src/mockApi');
+jest.mock('../src/mockApi');
 
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
@@ -24,6 +25,23 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const user = {
+    name: {
+      first: 'Antônio',
+      last: 'Britto'
+    },
+    gender: 'male',
+    location: {
+      country: 'Brazil'
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890'
+    }
+  }
+
+  api.fetchURL.mockImplementation(() => new Promise((resolve, reject) => resolve(user)));
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
