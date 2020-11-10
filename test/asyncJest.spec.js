6db5f3734b993585026d5aea69e1df9eca93bc7d
Isabella Paz <bellapaz.iorp@gmail.com>
2020-11-10 12:30:47 -0300

    Projeto Jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone('Alô?').then(answer => {expect(answer).toMatch('Oi!');});
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone().catch(answer => expect(answer).toMatch('Infelizmente não podemos atender...'));
   });
 });
