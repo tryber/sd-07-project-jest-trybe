e164149c64cce449a92c75f9ecdbfc98ea535c20
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-11-10 13:31:56 -0300

    iniciando requisito 4. Vamos com tudo! :rocket:

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+    console.log(adventure.specialists.length);
+  });
+  afterAll(() => {
+    console.log(adventure.specialists[0]);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
