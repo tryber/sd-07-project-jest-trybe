ab82b010caa135bcab0d12f6bd223297097d21d8
Alstent <rafaelasltent@gmail.com>
2020-11-11 11:27:50 +0100

    resolvendo requisito 2

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,9 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", async () => {
+  test("atende", async (done) => {
     const answer = await answerPhone(true);
     expect(answer).toBe('Oi!');
+    done();
     // !! ALTERNATIVA !!
     // expect(await answerPhone(true)).toBe('Oi!');
   });
