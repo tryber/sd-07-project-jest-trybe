428eee28caac797c6640877397f530112c83ccf5
Eduardo Pizorno <pizorno@gmail.com>
2020-11-12 09:36:10 -0300

    Jest Assincrono

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async(done) => {
+    expect.assertions(1);
+    const response = await answerPhone(true);
+    expect(response).toBe("Oi!");
+    done();
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", (done) => {
+    answerPhone(false).catch((error) => {
+      expect(error).toBe("Infelizmente não podemos atender...");
+    });
+    done();
   });
 });
