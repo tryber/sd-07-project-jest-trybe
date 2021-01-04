c3faa292729e88ee70cdafc4f392581765428b7e
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-01-04 02:10:38 -0300

    faltou o s

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,13 +13,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    //assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
     return expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
   test("ocupado", () => {
-    //assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
-    return expect(answerPhone(false)).reject.toEqual('Infelizmente não podemos atender...');
+    return expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
