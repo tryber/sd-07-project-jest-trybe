22bbe133438bcf9821e4de53e69e9dba52028150
Jorge Alves <alvesdjorge@gmail.com>
2021-01-04 13:53:06 -0300

    comite de recuperacao

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,10 +22,25 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+const objUser = {
+  gender: 'male',
+  name: {
+    first: 'Antônio',
+    last: 'Britto',
+  },
+  location: { country: 'Brazil' },
+  email: 'tunico@bol.com.br',
+  login: {
+    username: 'tunicao123',
+    password: '1234567890',
+  },
+}
+
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
 
   test('verifica se o usuário é o tunico', async () => {
+    api.fetchURL = jest.fn().mockResolvedValue(objUser);
     return api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
